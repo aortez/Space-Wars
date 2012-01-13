28f771dfc937c9bfcdfbe590a1cd0ef994bbec20
Allan Ortiz <aortez@gmail.com>
2012-01-12 22:03:11 -0800

    removed unnecessary include

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
-#include "Rect2f.h"
 
 
 int main(int argc, char *argv[])
 {
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
