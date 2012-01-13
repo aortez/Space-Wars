bc4be77376180832ff70eb233e8041a5d626b051
holland S <hgschutte1@gmail.com>
2012-01-12 19:45:07 -0800

    Adding rectangle files and Vec3 file

---
+++ Rect2f.cpp
@@ -0,0 +1,94 @@
+#include "Rect2f.h"
+
+/**********/
+/* PUBLIC */
+/**********/
+
+Rect2f::Rect2f( Vec2f center, float width, float height, float radius, Vec3f color)
+    : Shape( center, radius ),
+      mColor( color ),
+      mWidth( width ),
+      mHeight( height )
+{
+    mTopRightCrnr = new Vec2f( mCenter.X - width * 0.5f, mCenter.Y - height * 0.5f );
+    mTopLeftCrnr = new Vec2f( mCenter.X + width * 0.5f, mCenter.Y + height * 0.5f );
+    mBottomLeftCrnr = new Vec2f( mCenter.X - width * 0.5f, mCenter.Y + height * 0.5f );
+    mBottomRightCrnr = new Vec2f( mCenter.X + width * 0.5f, mCenter.Y - height * 0.5f );
+}
+
+Rect2f::~Rect2f() {
+
+}
+
+void Rect2f::Collide( Shape &s ) {
+    s.Collide( *this );
+}
+
+void Rect2f::Collide( Rect2f &r ) {
+
+    Vec2f Dn;
+
+    float delta = 0;
+
+    bool collides = false;
+
+    while ( delta == 0 ) {
+
+        r.mCenter.X += randcollider();
+        r.mCenter.Y += randcollider();
+
+        r.UpdateCorners();
+
+        Dn = mCenter - r.mCenter;
+
+        delta = Dn.magnitude();
+    }
+
+
+}
+
+void Rect2f::Collide( Circle& c ) {
+
+}
+
+bool Rect2f::Intersects( const Shape &s ) const {
+    return false;
+}
+
+bool Rect2f::Intersects( const Rect2f &s ) const {
+    return false;
+}
+
+bool Rect2f::IsAlive( void ) const {
+    return false;
+}
+
+float Rect2f::Mass( void ) const {
+    return -1.0f; //const float x = ( mWidthHeight->X *  )
+}
+
+void Rect2f::Draw( void ) const {
+
+    /*
+    mRadius =
+        ( ( mWidthHeight->X * mWidthHeight ) + ( mWidthHeight->Y * mWidthHeight->Y ) ) / 2;*/
+
+    const int numPoints = mRadius * 1000;
+    glBegin( GL_POLYGON_SMOOTH );
+    glColor3f( mColor.X, mColor.Y, mColor.Z );
+
+    for ( double i = 0; i < mRadius; i++ )
+    {
+        const float x = mCenter.X + std::cos( i / numPoints * 2 * PI ) * mRadius;
+        const float y = mCenter.Y + std::sin( i / numPoints * 2 * PI ) * mRadius;
+        glVertex3f( x, y, 0.0f );
+    }
+
+    glEnd();
+}
+
+/***********/
+/* PRIVATE */
+/***********/
+
+
--- Vec3.h
+++ Vec3.h
@@ -5,53 +5,53 @@
 #include <math.h>
 #include "enum.h"
 
-class Vector3f {
+class Vect3f {
 
 public:
         float X, Y, Z;
-        Vector3f(const float x, const float y, const float z);
-	~Vector3f();
+        Vect3f(const float x, const float y, const float z);
+        ~Vect3f();
 
         inline float getSum() const{
                 return X + Y + Z;
 	}
 
         float getMagnitude() const;
-        void normalize() const;
+        void normalize();
         //Float Operations
-        Vector3f operator/= (const float a);
-        Vector3f operator*= (const float a);
-        Vector3f operator+= (const float a);
-        Vector3f operator-= (const float a);
+        Vect3f& operator/= (const float a);
+        Vect3f& operator*= (const float a);
+        Vect3f& operator+= (const float a);
+        Vect3f& operator-= (const float a);
 
         //Vector Operations
-        Vector3f operator/= (const Vector3f& vector);
-        Vector3f operator*= (const Vector3f& vector);
-        Vector3f operator+= (const Vector3f& vector);
-        Vector3f operator-= (const Vector3f& vector);
+        Vect3f& operator/= (const Vect3f& vector);
+        Vect3f& operator*= (const Vect3f& vector);
+        Vect3f& operator+= (const Vect3f& vector);
+        Vect3f& operator-= (const Vect3f& vector);
 };
 
 //-------Float-------
 
-Vector3f operator+ (Vector3f a, const float b);
-Vector3f operator- (Vector3f a, const float b);
-Vector3f operator/ (Vector3f a, const float b);
-Vector3f operator* (Vector3f a, const float b);
+Vect3f& operator+ (Vect3f& a, const float b);
+Vect3f& operator- (Vect3f& a, const float b);
+Vect3f& operator/ (Vect3f& a, const float b);
+Vect3f& operator* (Vect3f& a, const float b);
 //------Vector--------
-Vector3f operator+ (Vector3f a, const Vector3f& b);
-Vector3f operator- (Vector3f a, const Vector3f& b);
-Vector3f operator/ (Vector3f a, const Vector3f& b);
-Vector3f operator* (Vector3f a, const Vector3f& b);
+Vect3f& operator+ (Vect3f& a, const Vect3f& b);
+Vect3f& operator- (Vect3f& a, const Vect3f& b);
+Vect3f& operator/ (Vect3f& a, const Vect3f& b);
+Vect3f& operator* (Vect3f& a, const Vect3f& b);
 
-float compdist3f(const Vector3f& vec_a, const Vector3f& vec_b);
+float compdist3f(const Vect3f& vec_a, const Vect3f& vec_b);
 
-float vectprod3f(const Vector3f& vecta,const Vector3f& vectb);
+float vectprod3f(const Vect3f& vecta,const Vect3f& vectb);
 
-float dotprod3f(const Vector3f& vecta, const Vector3f& vectb);
+float dotprod3f(const Vect3f& vecta, const Vect3f& vectb);
 
-AngleType getAngleType(const Vector3f& a, const Vector3f& b);
+AngleType getAngleType(const Vect3f& a, const Vect3f& b);
 
-float getAngle(const Vector3f& a, const Vector3f& b);
+float getAngle(const Vect3f& a, const Vect3f& b);
 
-float projvectf(const Vector3f& v, const Vector3f& n);
+float projvectf(const Vect3f& v, const Vect3f& n);
 #endif //VECTOR3F_H
